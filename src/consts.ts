26a1423a2f457d2264bb8e2ce909a53692541765
Mario Perez Hurtado <marioph10@gmail.com>
2023-07-08 19:45:06 +0200

    Added link component

--- src/consts.ts
+++ src/consts.ts
@@ -1,2 +1,2 @@
-export const SITE_TITLE = 'Mario Pérez Hurtado | Portfolio'
+export const SITE_TITLE = 'Mario Pérez Hurtado • Portfolio'
 export const SITE_DESCRIPTION = 'Frontend Developer & Tech Enthusiast.'
