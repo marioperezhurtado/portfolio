27df49bade51f2b5e99c6c42535397122a4438f5
Mario Perez Hurtado <marioph10@gmail.com>
2023-07-09 13:36:53 +0200

    Added social images, rss and style changes

--- src/components/Tool.svelte
+++ src/components/Tool.svelte
@@ -8,7 +8,7 @@
 <div
   class="bg-pf-dark-2 h-44 aspect-video p-4 flex flex-col justify-between rounded-md shadow-md"
 >
-  <h3 class="text-lg sm:text-xl font-bold font-display text-pf-light">
+  <h3 class="text-lg font-semibold text-pf-light">
     {title}
   </h3>
   <p class="font-semibold">
--- src/content/config.ts
+++ src/content/config.ts
@@ -13,7 +13,8 @@ const blog = defineCollection({
     updatedDate: z
       .string()
       .optional()
-      .transform((str) => (str ? new Date(str) : undefined))
+      .transform((str) => (str ? new Date(str) : undefined)),
+    image: z.string()
   })
 })
 
