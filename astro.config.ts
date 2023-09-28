2ce4b7546293aa9874e1697d16cec128435096c6
Mario Perez Hurtado <marioph10@gmail.com>
2023-09-28 18:33:00 +0200

    feat: add line highlight

--- astro.config.ts
+++ astro.config.ts
@@ -2,15 +2,19 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 import mdx from '@astrojs/mdx'
 import svelte from '@astrojs/svelte'
+import rehypePrettyCode, {type Options } from 'rehype-pretty-code'
+
+const prettyCodeOptions = {
+  theme: 'poimandres',
+} satisfies Options;
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://marioph.com',
   integrations: [
     mdx({
-      syntaxHighlight: 'shiki',
-      shikiConfig: { theme: 'poimandres' },
-      gfm: false
+      syntaxHighlight: false,
+      rehypePlugins: [[rehypePrettyCode, prettyCodeOptions]],
     }),
     tailwind(),
     svelte(),
