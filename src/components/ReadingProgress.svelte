cb7d96c8b2e9ed4b26e53735f45eb3331d31c32e
Mario Perez Hurtado <marioph10@gmail.com>
2023-10-08 17:58:23 +0200

    feat: add reading progress indicator

---
+++ src/components/ReadingProgress.svelte
@@ -0,0 +1,20 @@
+<script lang="ts">
+  let progress = 0;
+
+  function handleScroll() {
+    const winScroll = document.documentElement.scrollTop;
+    const height =
+      document.documentElement.scrollHeight -
+      document.documentElement.clientHeight;
+    progress = (winScroll / height) * 100;
+  }
+
+</script>
+
+<svelte:window on:scroll={handleScroll}/>
+
+<div class="fixed top-0 left-0 w-screen">
+  <span class="bg-pf-dark-3 h-2 absolute top-0 rounded-r-sm" 
+    style="width: {progress}%;"
+  />
+</div>
